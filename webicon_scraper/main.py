ff6c38534b76fbe4c015218c38102bc4f5bcf85c
tridn <tridnguyen97@gmail.com>
2025-03-23 17:12:36 +0700

    change file store location

--- webicon_scraper/main.py
+++ webicon_scraper/main.py
@@ -4,7 +4,7 @@ from scrapy.settings import Settings
 
 
 def run() -> None:
-    store_uri = "file://./assets"
+    store_uri = "file://./results"
     settings = Settings()
     settings.set("ITEM_PIPELINES", {"webicon_scraper.pipeline.FavIconPipeline": 1})
     settings.set("FILES_STORE", store_uri)
